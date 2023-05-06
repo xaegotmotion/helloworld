2bb6a0fc546340e6f17a0e8e2690e55559ccf4a8
SaintLupe <51855473+hsccdc@users.noreply.github.com>
2023-05-06 13:43:13 -0400

    completed to-do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [list, setList] = useState([]); 
+  const [toDo,settoDo] = useState("Want some Cool H-Whip?");
+  useEffect(()=> {
+    setList(["stewieWorldDominationShallBeMine","eat1Donut","pleaseLetsJustGoHome"]) 
+  }, [])
+  const deletetoDo = (val) => {setList(
+
+  )};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, World!</h1>
+      <h2>Today, we will acheive...</h2>
+
+      <ul>
+        {list.map((val, id) => {return <li onClick = {
+          () => {setList(
+            list.filter((item) => {return item !== val})
+          )}
+        } key = {id}>{val}</li>}) }
+      </ul>
+      <input val = {toDo} onChange = {(e)=>{settoDo(e.target.value);}}/>
+      <button onClick = {()=>{setList([...list, toDo])}}>Add to-do</button>
     </div>
   );
 }
